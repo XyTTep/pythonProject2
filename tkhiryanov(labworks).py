53b710aeda1cec10f5d2122ff0da1811cbde9d65
xyttep <Xyttep@gmail.com>
2021-05-09 23:52:54 +0300

    added square_spiral function =)

--- tkhiryanov(labworks).py
+++ tkhiryanov(labworks).py
@@ -1,33 +1,31 @@
 import turtle
-
-# window = turtle.Screen()
 turtle.shape('turtle')
 
 
-# def square(side, number_of_squares):
-#     """draw square
-#     side = first size of the edge"""
-#     for a in range(1, number_of_squares + 1):
-#         for i in range(4):
-#             turtle.left(90)
-#             turtle.forward(side * a)
-#         turtle.penup()
-#         turtle.forward(side * 0.5)
-#         turtle.right(90)
-#         turtle.forward(side * 0.5)
-#         turtle.left(90)
-#         turtle.pendown()
+def square_in_square(side, number_of_squares):
+    """draw square
+    side = first size of the edge"""
+    for a in range(1, number_of_squares + 1):
+        for i in range(4):
+            turtle.left(90)
+            turtle.forward(side * a)
+        turtle.penup()
+        turtle.forward(side * 0.5)
+        turtle.right(90)
+        turtle.forward(side * 0.5)
+        turtle.left(90)
+        turtle.pendown()
 #
 #
-# def spider(length, number_of_legs):
-#     """draw square
-#     side = first size of the edge"""
-#     for a in range(1, number_of_legs + 1):
-#         angle_ = 360 // number_of_legs
-#         turtle.forward(length)
-#         turtle.left(180)
-#         turtle.forward(length)
-#         turtle.left(180 + angle_)
+def spider(length, number_of_legs):
+    """draw square
+    side = first size of the edge"""
+    for a in range(1, number_of_legs + 1):
+        angle_ = 360 // number_of_legs
+        turtle.forward(length)
+        turtle.left(180)
+        turtle.forward(length)
+        turtle.left(180 + angle_)
 
 
 def spiral_():
@@ -39,11 +37,33 @@ def spiral_():
         turtle.forward(a)
         turtle.left(1)
 
+def square_spiral(first_side):
+    """draw square spiral
+    first_side = size of the first edge"""
+    a = 1
+    for a in range(20):
+        for i in range(2):
+            turtle.left(90)
+            turtle.forward(first_side * a)
+        a += 1
+
+square_in_square(20, 3)
+turtle.goto(0, 0)
+turtle.clear()
 
 
-# square(20, 3)
-# turtle.clear()
+spider(50, 10)
+turtle.goto(0, 0)
+turtle.clear()
 
 window = turtle.Screen()
-spiral_()
+square_spiral(10)
 window.exitonclick()
+
+spiral_()
+turtle.goto(0, 0)
+turtle.clear()
+
+
+
+
