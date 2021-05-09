c8120ffb87d776e51d6ab426cd814322ccae44f4
xyttep <Xyttep@gmail.com>
2021-05-09 21:16:00 +0300

    added spiral function =)

--- tkhiryanov(labworks).py
+++ tkhiryanov(labworks).py
@@ -4,35 +4,46 @@ import turtle
 turtle.shape('turtle')
 
 
-def square(side, number_of_squares):
+# def square(side, number_of_squares):
+#     """draw square
+#     side = first size of the edge"""
+#     for a in range(1, number_of_squares + 1):
+#         for i in range(4):
+#             turtle.left(90)
+#             turtle.forward(side * a)
+#         turtle.penup()
+#         turtle.forward(side * 0.5)
+#         turtle.right(90)
+#         turtle.forward(side * 0.5)
+#         turtle.left(90)
+#         turtle.pendown()
+#
+#
+# def spider(length, number_of_legs):
+#     """draw square
+#     side = first size of the edge"""
+#     for a in range(1, number_of_legs + 1):
+#         angle_ = 360 // number_of_legs
+#         turtle.forward(length)
+#         turtle.left(180)
+#         turtle.forward(length)
+#         turtle.left(180 + angle_)
+
+
+def spiral_():
     """draw square
     side = first size of the edge"""
-    for a in range(1, number_of_squares + 1):
-        for i in range(4):
-            turtle.left(90)
-            turtle.forward(side * a)
-        turtle.penup()
-        turtle.forward(side * 0.5)
-        turtle.right(90)
-        turtle.forward(side * 0.5)
-        turtle.left(90)
-        turtle.pendown()
+    a = 0
+    while a < 2:
+        a += 0.001
+        turtle.forward(a)
+        turtle.left(1)
 
 
-def spider(length, number_of_legs):
-    """draw square
-    side = first size of the edge"""
-    for a in range(1, number_of_legs + 1):
-        angle_ = 360 // number_of_legs
-        turtle.forward(length)
-        turtle.left(180)
-        turtle.forward(length)
-        turtle.left(180 + angle_)
 
-
-square(20, 3)
-turtle.clear()
+# square(20, 3)
+# turtle.clear()
 
 window = turtle.Screen()
-spider(100, 6)
+spiral_()
 window.exitonclick()
