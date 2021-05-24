6bf99a9e45fb452c73473ddbb96675f684071c34
Zmitrovich <xyttep@gmail.com>
2021-05-24 16:18:35 +0300

    realization of the smile func

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -7,24 +7,28 @@ FPS = 30
 screen = pygame.display.set_mode((600, 600))
 
 x = 300
-y = 300
-r = 200
+y = 200
+r = 320
 N = 10
 color = (255, 207, 72)
 eyes_color = (189, 218, 87)
 left_eye_position_x = x - ((r / 2) * 0.8)
 eye_position_y = y - ((r / 2) * 0.5)
 right_eye_position_x = x + ((r / 2) * 0.8)
-
+# drawing part
 circle(screen, color, (x, y), r)
-circle(screen, ('black'), (left_eye_position_x, eye_position_y), r * 0.31)
-circle(screen, ('black'), (right_eye_position_x, eye_position_y), r * 0.21)
+circle(screen, 'black', (left_eye_position_x, eye_position_y), r * 0.31)
+circle(screen, 'black', (right_eye_position_x, eye_position_y), r * 0.21)
 circle(screen, eyes_color, (left_eye_position_x, eye_position_y), r * 0.3)
 circle(screen, eyes_color, (right_eye_position_x, eye_position_y), r * 0.2)
-circle(screen, ('black'), (left_eye_position_x, eye_position_y), r * 0.08)
-circle(screen, ('black'), (right_eye_position_x, eye_position_y), r * 0.08)
-rect(screen, ('black'), (left_eye_position_x + r*0.31, eye_position_y - r*0.31, -200, -180),20) #nerabochee govno
-
+circle(screen, 'black', (left_eye_position_x, eye_position_y), r * 0.08)
+circle(screen, 'black', (right_eye_position_x, eye_position_y), r * 0.08)
+line(screen, 'black', (left_eye_position_x + r * 0.4, eye_position_y - r * 0.2),
+     (left_eye_position_x - r * 0.4, eye_position_y - r * 0.5), 20)
+line(screen, 'black', (right_eye_position_x - r * 0.2, eye_position_y - r * 0.2),
+     (right_eye_position_x + r * 0.4, eye_position_y - r * 0.5), 20)
+line(screen, 'black', (x + r * 0.4, y + r * 0.5),
+     (x - r * 0.4, y + r * 0.5), 30)
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
